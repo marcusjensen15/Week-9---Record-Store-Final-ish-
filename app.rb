47bc9bff3adbdca0e9f96ded0da943801a5a29db
Student <example@example.com>
2019-12-18 11:46:12 -0800

    update functionality

--- app.rb
+++ app.rb
@@ -1,32 +1,31 @@
 require('sinatra')
 require('sinatra/reloader')
-require('./lib/project')
+require('./lib/album')
+require('./lib/song')
 require('pry')
 also_reload('lib/**/*.rb')
 
 get('/test/') do
-   @something = "this is a variable"
+  @something = "this is a variable"
   erb(:whatever)
 end
 
 get('/') do
-  @sort = Album.sort
   @albums = Album.all
   @sold = Album.sold_albums
-
   erb(:albums)
 end
 
 get('/albums') do
-  if params['name']
-    @album = Album.search(params[:name])
-    erb(:album)
-else
-  @sort = Album.sort
   @albums = Album.all
   @sold = Album.sold_albums
   erb(:albums)
-  end
+end
+
+post('/albums/search') do
+  @albums = Album.search(params[:type], params[:search])
+  @sold = Album.sold_albums
+  erb(:albums)
 end
 
 get('/albums/new') do
@@ -37,14 +36,6 @@ get('/albums/:id') do
   @album = Album.find(params[:id].to_i())
   erb(:album)
 end
-
-##this is new
-
-
-
-
-#this is new ^
-
 
 get('/albums/:id/edit') do
   @album = Album.find(params[:id].to_i())
@@ -58,72 +49,87 @@ post('/albums') do
   genre = params[:genre]
   album = Album.new(name, nil, year, genre, artist)
   album.save()
-  @albums = Album.all()
-  @sort = Album.sort
+  @albums = Album.all
   @sold = Album.sold_albums
-
   erb(:albums)
 end
 
-patch('/update/:id')do
-@album = Album.find(params[:id].to_i())
-@album.sold
-
-@albums = Album.all
-@sort = Album.sort
-@sold = Album.sold_albums
-
-erb(:albums)
+patch('/albums/:id/sold') do
+  @album = Album.find(params[:id].to_i())
+  @album.sold
+  @albums = Album.all
+  @sold = Album.sold_albums
+  erb(:albums)
 end
 
-get('/update/:id')do
-
-
-@albums = Album.all
-@sort = Album.sort
-@sold = Album.sold_albums
-
-erb(:albums)
+get('/update/:id') do
+  @albums = Album.all
+  @sold = Album.sold_albums
+  erb(:albums)
 end
-
 
 patch('/albums/:id') do
-
-  # if params["sold"]
-    # @album = Album.find(params[:id].to_i())
-    # @album.sold
-    # @albums = Album.all
-    # @sort = Album.sort
-    # @sold = Album.sold_albums
-
-
-    # erb(:albums)
-
-
-
-# else
   @album = Album.find(params[:id].to_i())
   @album.update(params[:name])
   @album.sold
-
   @albums = Album.all
-  @sort = Album.sort
   @sold = Album.sold_albums
-
   erb(:albums)
-# end
 end
 
 delete('/albums/:id') do
   @album = Album.find(params[:id].to_i())
   @album.delete()
   @albums = Album.all
-  @sort = Album.sort
   @sold = Album.sold_albums
-
   erb(:albums)
 end
 
-get('/custom_route') do
-  "We can even create custom routes, but we should only do this when needed."
+#### Routing for songs below
+
+get('/albums/:id/songs') do
+  @album = Album.find(params[:id].to_i())
+  @songs = @album.get_songs
+  erb(:songs)
+end
+
+get('/albums/:id/songs/new') do
+  @album = Album.find(params[:id].to_i())
+  erb(:new_song)
+end
+
+post('/albums/:id/songs') do
+  @album = Album.find(params[:id].to_i())
+  song = Song.new(params[:song_name], @album.id, nil)
+  song.save
+  @songs = @album.get_songs
+  erb(:songs)
+end
+
+get('/albums/:id/songs/:song_id') do
+  @album = Album.find(params[:id].to_i())
+  @song = Song.find(params[:song_id].to_i())
+  erb(:song)
+end
+
+get('/albums/:id/songs/:song_id/edit') do
+  @album = Album.find(params[:id].to_i())
+  @song = Song.find(params[:song_id].to_i())
+  erb(:edit_song)
+end
+
+delete('/albums/:id/songs/:song_id') do
+  @album = Album.find(params[:id].to_i())
+  song = Song.find(params[:song_id].to_i())
+  song.delete
+  @songs = @album.get_songs
+  erb(:songs)
+end
+
+patch('/albums/:id/songs/:song_id') do
+  @album = Album.find(params[:id].to_i())
+  song = Song.find(params[:song_id].to_i())
+  song.update(params[:name])
+  @songs = @album.get_songs
+  erb(:songs)
 end
